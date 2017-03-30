03d78e7353157c9d5a58c0507612a1c3cdaf3522
Hack Reactor Student <student@hackreactor.com>
2017-03-29 20:31:18 -0700

    Completed graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,35 +1,68 @@
-
-
 // Instantiate a new graph
 var Graph = function() {
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+	this[node] = {};
+	this[node]['edge'] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+	if(this.hasOwnProperty(node)) {
+		return true;
+	}
+	return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+	for (var key in this) {
+		var currentKey = this[key];
+		if (currentKey['edge']){
+			if (currentKey['edge'].includes(node)) {
+				var index = currentKey['edge'].indexOf(node);
+				currentKey['edge'].splice(index, 1); 
+			}
+		}
+	}
+	delete this[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+	if (this[fromNode]['edge'].indexOf(toNode) !== -1)
+		return true;
+	return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+	this[fromNode]['edge'].push(toNode);
+	this[toNode]['edge'].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+	//if edge exists
+	if(this[fromNode]['edge'].indexOf(toNode) !== -1) {
+		//remove edge from fromNode
+		var index = this[fromNode]['edge'].indexOf(toNode);
+		this[fromNode]['edge'].splice(index,1);
+		//remove edge from toNode
+		var index2 = this[toNode]['edge'].indexOf(fromNode);
+		this[toNode]['edge'].splice(index2,1);		
+	}
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+	for (var key in this) {
+		if (this.hasOwnProperty(key)){
+			cb(key);		
+		}
+	}
 };
 
 /*
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,10 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
+  _.extend(newTree,treeMethods);
+
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,9 +13,31 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+	var newChildObj = Tree(value); 
+	this.children.push(newChildObj);
 };
 
 treeMethods.contains = function(target) {
+	//IF target is equal this.value
+	if (target === this.value){
+		//return true
+		return true;
+	}
+
+	//RECURSIVE CASE
+	//if this.children.length is > 0
+	if (this.children.length > 0) {
+		//FOR loop thru its
+		for (var i=0; i < this.children.length; i++){
+			//recursively calling on the children[i].contains(target)
+			if (this.children[i].contains(target)) {
+				return true;
+			};
+		}	
+	}
+
+	//return false
+	return false;
 };
 
 
