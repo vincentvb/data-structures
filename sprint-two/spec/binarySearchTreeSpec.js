7f5ae25454ca0517f2df214c29c3d4fe261af20c
Hack Reactor Student <student@hackreactor.com>
2017-03-30 16:25:48 -0700

    Completed BinarySearchTree

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -2,7 +2,7 @@ describe('binarySearchTree', function() {
   var binarySearchTree;
 
   beforeEach(function() {
-    binarySearchTree = BinarySearchTree(5);
+    binarySearchTree = new BinarySearchTree(5);
   });
 
   it('should have methods named "insert", "contains", and "depthFirstLog', function() {
@@ -32,7 +32,7 @@ describe('binarySearchTree', function() {
     var array = [];
     var func = function(value) { array.push(value); };
     binarySearchTree.insert(2);
-    binarySearchTree.insert(3);
+    binarySearchTree.insert(3);      
     binarySearchTree.depthFirstLog(func);
     expect(array).to.eql([5, 2, 3]);
   });
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,75 @@
 var BinarySearchTree = function(value) {
+	this.value = value;
+	this.left = null;
+	this.right = null;
 };
+
+
+BinarySearchTree.prototype.insert = function (value) { 
+	//compare newNode to parent's value (this.value) 
+	//if < parent's value 
+	if (value < this.value) {
+		//this.left === null  
+		if (this.left === null){
+			//set to newNode
+			this.left = new BinarySearchTree(value);
+		}
+		//this.left is defined 
+		if (value !== null){
+			//recursively call ____.insert(value);
+			this.left.insert(value);
+		}
+	}
+
+	//if > parent's value 
+	if (value > this.value) {
+		//this.right === null  
+		if (this.right === null){
+			//set to newNode
+			this.right = new BinarySearchTree(value);
+		}
+		//this.right is defined 
+		if (this.right !== null){
+			//recursively call ____.insert(value);
+			this.right.insert(value);
+		}
+	}
+};
+    
+BinarySearchTree.prototype.contains = function (value) {
+	if (value === this.value){
+		return true;
+	}
+
+	//if < parent's value 	
+	if (value < this.value && this.left !== null) {
+			return this.left.contains(value);
+	}
+
+	//if > parent's value 
+	if (value > this.value && this.right !== null) {
+	 		return this.right.contains(value);
+	}
+
+	return false;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function (cb) {
+	cb(this.value)  //push 5
+
+	//if this.left exists
+	if (this.left !==null){
+		//__this.left____.depthFirstLog(cb)  //push 2
+		this.left.depthFirstLog(cb);
+	}
+
+	// if this.right exists
+	if (this.right !== null) {
+		//__this.right__.detphFirstLog(cb)  //push 7
+		this.right.depthFirstLog(cb);		
+	}
+};
+
 
 
 /*
