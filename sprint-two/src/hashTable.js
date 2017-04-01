8e21b1d066680af574cce2a3b2c7a4790c7e5a34
Hack Reactor Student <student@hackreactor.com>
2017-03-31 22:03:05 -0700

    Refactored hashTable (75% resizing)

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,51 +1,55 @@
-
-
 var HashTable = function(limit, storage) {
   this._limit = limit || 8
   this._storage = storage || LimitedArray(this._limit);
   this._tuples = 0 
 };
 
-//if 
 //use LimitedArray's .set (this._storage.set(index, {k:v}))
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   this._tuples++;
 
+  //****** NORMAL INSERT (no resizing) ******
+  var insertTupal = function () {
+    if (this._storage.get(index) === undefined) {
+      var tupal = [k,v]; 
+      var bucket = [tupal];
+      this._storage.set(index, bucket);
+    } else {
+      var currentArray = this._storage.get(index); 
 
-  //*** IF >75% capacity
-  //if index equals undefined, create array and insert. otherwise push to current
-  if ((this._tuples / this._limit) > 0.75) {
-      if (this._storage.get(index) === undefined) {
-        var tupal = [k,v]; 
-        var bucket = [tupal];
-        this._storage.set(index, bucket);
-  }
-      else {
-        var currentArray = this._storage.get(index); 
-
-
-        for (var i=0; i<currentArray.length; i++){
-        //check if currentArray contains k
+      for (var i=0; i<currentArray.length; i++){
+        //check if currentArray contains k - replace exisiting value 
         if (currentArray[i][0] === k){
           currentArray[i] = [k, v];
         } else {
           tupal = [k, v];
           currentArray.push(tupal);     
         }
-    }
-  }
+      }
+    }    
+  };
+
+  //invoke insertTupal to insert
+  insertTupal.call(this);
+
+  //****** IF >75% CAPACITY (RESIZE) ******
+  if ((this._tuples / this._limit) > 0.75) {
+    //insert last tupal that is greater than 75%
+    insertTupal.call(this);
+
+    //retrieve all tupals in the hashtable and push to storageVals
     var storageVals = [];
     var hashCopy = this;
-    
-    this._storage.each(function (storage) {
-      if (storage !== undefined){
-        for (var i = 0; i < storage.length; i++) {
-          storageVals.push([storage[i][0], hashCopy.retrieve(storage[i][0])]);
+    this._storage.each(function (bucket) {
+      if (bucket !== undefined){
+        for (var i = 0; i < bucket.length; i++) {
+          storageVals.push([bucket[i][0], hashCopy.retrieve(bucket[i][0])]);
         }
       }
     });
 
+    //clear hashtable
     this._storage.each(function(bucket) {
       if (bucket !== undefined){
         for (var i = 0; i < bucket.length; i++) {
@@ -59,34 +63,15 @@ HashTable.prototype.insert = function(k, v) {
       }
     });
 
+    //double limit. call LimitedArray to increase storage's limit. reset tuples count. 
     this._limit = this._limit * 2;
     this._storage = LimitedArray(this._limit);
     this._tuples = 0;
 
+    //push all tupals in storageVals into new increase sized hashtable 
     for (var i = 0; i < storageVals.length; i++) {
       this.insert(storageVals[i][0], storageVals[i][1]);
     }
-  }
-
-    //****** OLD INSERT
-  if (this._storage.get(index) === undefined) {
-    var tupal = [k,v]; 
-    var bucket = [tupal];
-	  this._storage.set(index, bucket);
-	}
-  else {
-  	var currentArray = this._storage.get(index); 
-
-
-  	for (var i=0; i<currentArray.length; i++){
-	  	//check if currentArray contains k
-	  	if (currentArray[i][0] === k){
-	  		currentArray[i] = [k, v];
-	  	} else {
-		  	tupal = [k, v];
-		  	currentArray.push(tupal);  		
-	  	}
-  	}
   }
 };
 
@@ -98,16 +83,15 @@ HashTable.prototype.retrieve = function(k) {
   var bucket = this._storage.get(index);
   
   //if bucket is undefined
-  if (bucket === undefined){
+  if (bucket === undefined) {
   	return undefined;
   } else {
-  	for (var i=0; i<bucket.length; i++){
+  	for (var i=0; i<bucket.length; i++) {
   		if (bucket[i][0] === k){
   			return bucket[i][1];  	
   		}
   	}
   }
-
 };
 
 //use LimitedArray's get and set 
